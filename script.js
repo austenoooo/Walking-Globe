3ed17ef7d2ad101a7c99c2ac87e54db27156ed8d
austenoooo <yl5661@nyu.edu>
2023-02-11 12:20:46 -0500

    add textures

--- script.js
+++ script.js
@@ -1,92 +1,191 @@
-const ground_radius = 200;
-const view_length = 20;
-const height = 2;
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+// in order to use an HDR image, we need to first load the RGBELoader
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
+
+
+// define movement/ratio parameters
+const ground_radius = 200; // radius of the ground sphere
+const view_length = 20; // the length of the tangent line (viewpoint)
+const height = 2; // height of the camera to the groud surface
 let camera_pos = {
   x: ground_radius + height,
   y: 0,
   z: 0,
-};
+}; // camera position
 let camera_look = {
   x: 0,
   y: view_length,
   z: 0,
-};
-
-let currentAngle = 0;
-let rotateSpeed = Math.PI / 1200;
-
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth/window.innerHeight,
-  0.1,
-  1000
-);
+}; // camera look at
+let currentAngle = 0; // starting rotation angle
+let rotateSpeed = Math.PI / 4800; // rotation speed
 
 
-camera.position.set(300, 300, 300);
-camera.lookAt(0, 0, 0);
+let scene, camera, renderer;
+// materail
+let materialReflective, materialGround;
+// geometry
+let groundMesh;
+let buildings = [];
+// texture
+let skyTexture;
 
-// for debug
-// camera.position.set(camera_pos.x, camera_pos.y, camera_pos.z);
-// camera.lookAt(camera_look.x, camera_look.y, camera_look.z);
+// build the scene
+function init(){
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth/window.innerHeight,
+    0.1,
+    1000
+    );
+    
+    // for debug
+    camera.position.set(300, 300, 300);
+    camera.lookAt(0, 0, 0);
 
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+    
+    // camera.position.set(camera_pos.x, camera_pos.y, camera_pos.z);
+    // camera.lookAt(camera_look.x, camera_look.y, camera_look.z);
 
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-// load environment map & sky box
+    // helper functions
+    // add orbit control
+    let controls = new OrbitControls(camera, renderer.domElement);
 
-const imgArray = [
-  "px.png",
-  "nx.png",
-  "py.png",
-  "ny.png",
-  "pz.png",
-  "nz.png",
-];
+    // add skybox and environment map
+    environmentMap();
 
-// load cubemap
-var cubeLoader = new THREE.CubeTextureLoader();
-cubeLoader.setPath('textures/');
-var textureCube = cubeLoader.load(imgArray);
+    // add geometry
+    createGeometry();
 
-
-// create material
-let materialReflective = new THREE.MeshStandardMaterial({
-    color: 0xffffff,
-    roughness: 0,
-    metalness: 1,
-    envMap: textureCube,
-  });
-
-
-// create ground
-let ground = new THREE.SphereGeometry(ground_radius, 64, 32);
-let groundMesh = new THREE.Mesh(ground, materialReflective);
-scene.add(groundMesh);
-groundMesh.position.set(0, 0, 0);
-
-// create buildings
-// randomly generate 100 box as buildings
-for (let i = 0; i < 100; i++) {
-  let x = Math.random() * 2 * Math.PI;
-  let y = Math.random() * 2 * Math.PI;
-  let z = Math.random() * 2 * Math.PI;
-  let buildingHeight = Math.random(0, 1) * 30 + 20 + ground_radius;
-  
-  let building = new THREE.BoxGeometry(
-    Math.random() * 4 + 4,
-    buildingHeight * 2,
-    Math.random() * 4 + 4
-  );
-  let buildingMesh = new THREE.Mesh(building, materialReflective);
-  scene.add(buildingMesh);
-
-  buildingMesh.position.set(0, 0, 0);
-  buildingMesh.rotation.set(x, y, z);
+    loop();
 }
+
+
+function environmentMap(){
+
+    let loader = new RGBELoader();
+    loader.load("./textures/environment.hdr", (texture) => {
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+        skyTexture = texture;
+        scene.background = skyTexture;
+    });
+
+}
+
+
+function createGeometry(){
+    // create material
+
+    const imgArray = [
+        "px.png",
+        "nx.png",
+        "py.png",
+        "ny.png",
+        "pz.png",
+        "nz.png",
+      ];
+      
+    // load cubemap
+    var cubeLoader = new THREE.CubeTextureLoader();
+    cubeLoader.setPath('./textures/');
+    var textureCube = cubeLoader.load(imgArray);
+
+    // create a texture loader:
+    let textureLoader = new THREE.TextureLoader();
+
+    let buildingNormal = textureLoader.load("./textures/010_normal.jpg");
+    let buildingRoughness = textureLoader.load("./textures/010_roughness.jpg");
+    let buildingHeight = textureLoader.load("./textures/010_height.png");
+    let buildingAmbient = textureLoader.load("./textures/010_ambientOcclusion.jpg");
+
+    buildingNormal.wrapS = THREE.RepeatWrapping;
+    buildingNormal.wrapT = THREE.RepeatWrapping;
+    buildingNormal.repeat.set(1, 30);
+
+    buildingRoughness.wrapS = THREE.RepeatWrapping;
+    buildingRoughness.wrapT = THREE.RepeatWrapping;
+    buildingRoughness.repeat.set(1, 30);
+
+    buildingHeight.wrapS = THREE.RepeatWrapping;
+    buildingHeight.wrapT = THREE.RepeatWrapping;
+    buildingHeight.repeat.set(1, 30);
+
+    materialReflective = new THREE.MeshStandardMaterial({
+        color: 0xffffff,
+        roughness: 0,
+        metalness: 1,
+        envMap: textureCube,
+        transparent: true,
+        opacity: 0.5,
+        reflectivity: 1,
+        normalMap: buildingNormal,
+        roughnessMap: buildingRoughness,
+        displacementMap: buildingHeight,
+        displacementScale: 0
+    });
+
+    
+    let groundTexture = textureLoader.load("./textures/Ice_001_COLOR.jpg");
+    let groundSpec = textureLoader.load("./textures/Ice_001_SPEC.jpg")
+
+    // change the texture parameters
+    groundTexture.wrapS = THREE.RepeatWrapping;
+    groundTexture.wrapT = THREE.RepeatWrapping;
+    groundTexture.repeat.set(50, 50);
+
+    groundSpec.wrapS = THREE.RepeatWrapping;
+    groundSpec.wrapT = THREE.RepeatWrapping;
+    groundSpec.repeat.set(50, 50);
+
+    materialGround = new THREE.MeshPhysicalMaterial({
+        color: 0xffffff,
+        roughness: 0,
+        metalness: 1,
+        envMap: textureCube,
+        transparent: true,
+        opacity: 0.8,
+        reflectivity: 1,
+        map: groundTexture,
+        specularMap: groundSpec,
+    });
+
+
+    // create ground
+    groundMesh = new THREE.Mesh(new THREE.SphereGeometry(ground_radius, 64, 32), materialGround);
+    scene.add(groundMesh);
+    groundMesh.position.set(0, 0, 0);
+
+    // create buildings
+    // randomly generate 100 box as buildings
+    for (let i = 0; i < 100; i++) {
+        let x = Math.random() * 2 * Math.PI;
+        let y = Math.random() * 2 * Math.PI;
+        let z = Math.random() * 2 * Math.PI;
+        let buildingHeight = Math.random(0, 1) * 30 + 20 + ground_radius;
+        
+        let building = new THREE.BoxGeometry(
+            Math.random() * 4 + 4,
+            buildingHeight * 2,
+            Math.random() * 4 + 4
+        );
+        let buildingMesh = new THREE.Mesh(building, materialReflective);
+        scene.add(buildingMesh);
+
+        buildingMesh.position.set(0, 0, 0);
+        buildingMesh.rotation.set(x, y, z);
+
+        buildings.push(buildingMesh);
+    }
+}
+
+
 
 
 
@@ -101,8 +200,6 @@ function loop() {
   // rinse and repeat
   window.requestAnimationFrame(loop);
 }
-
-loop();
 
 
 
@@ -132,3 +229,5 @@ function updateCamera() {
   camera_look.y = longRadius * Math.sin(currentAngle + addAngle);
   camera.lookAt(camera_look.x, camera_look.y, camera_look.z);
 }
+
+init();
